a2e06cb921ad6e161d1ae4a455cc2b3a5b9245f8
Alfian Ihsani Akmal <ihsani.akmal.0@gmail.com>
2025-07-01 14:25:36 +0700

    remapping nvim tree

--- configs/nvim/lua/mappings.lua
+++ configs/nvim/lua/mappings.lua
@@ -99,6 +99,7 @@ map("", "<C-=>", "<C-x>")
 map({ "i", "v" }, "<C-=>", "<Esc><C-x>a")
 map("n", "<leader>`", function() require("lazy").profile() end, { desc = "View lazy profile" })
 
+-- open Nvimtree window
 map({ "n", "x" }, "<C-m>", "<cmd>NvimTreeToggle<CR>")
 -- NvimTree mappings
 local function my_on_attach(bufnr)
@@ -109,9 +110,9 @@ local function my_on_attach(bufnr)
   end
 
   -- BEGIN_DEFAULT_ON_ATTACH
-  map("n", "O", api.tree.change_root_to_node, opts("CD"))
-  map("n", "o", api.node.open.replace_tree_buffer, opts("Open: In Place"))
-  map("n", "<C-i>", api.node.show_info_popup, opts("Info"))
+  map("n", "<C-i>", api.tree.change_root_to_node, opts("CD"))
+  map("n", "<C-e>", api.node.open.replace_tree_buffer, opts("Open: In Place"))
+  map("n", "z", api.node.show_info_popup, opts("Info"))
   map("n", "<C-r>", api.fs.rename_sub, opts("Rename: Omit Filename"))
   map("n", "<C-t>", api.node.open.tab, opts("Open: New Tab"))
   map("n", "<C-v>", api.node.open.vertical, opts("Open: Vertical Split"))
@@ -119,8 +120,8 @@ local function my_on_attach(bufnr)
   map("n", "<BS>", api.node.navigate.parent_close, opts("Close Directory"))
   map("n", "<CR>", api.node.open.edit, opts("Open"))
   map("n", "<Tab>", api.node.open.preview, opts("Open Preview"))
-  map("n", ">", api.node.navigate.sibling.next, opts("Next Sibling"))
-  map("n", "<", api.node.navigate.sibling.prev, opts("Previous Sibling"))
+  map("n", "i", api.node.navigate.sibling.next, opts("Next Sibling"))
+  map("n", "n", api.node.navigate.sibling.prev, opts("Previous Sibling"))
   map("n", ".", api.node.run.cmd, opts("Run Command"))
   map("n", "-", api.tree.change_root_to_parent, opts("Up"))
   map("n", "a", api.fs.create, opts("Create File Or Directory"))
@@ -134,55 +135,55 @@ local function my_on_attach(bufnr)
   map("n", "]c", api.node.navigate.git.next, opts("Next Git"))
   map("n", "d", api.fs.remove, opts("Delete"))
   map("n", "D", api.fs.trash, opts("Trash"))
-  map("n", "E", api.tree.expand_all, opts("Expand All"))
-  map("n", "e", api.fs.rename_basename, opts("Rename: Basename"))
+  map("n", "O", api.tree.expand_all, opts("Expand All"))
+  map("n", "w", api.fs.rename_basename, opts("Rename: Basename"))
   map("n", "]e", api.node.navigate.diagnostics.next, opts("Next Diagnostic"))
   map("n", "[e", api.node.navigate.diagnostics.prev, opts("Prev Diagnostic"))
   map("n", "F", api.live_filter.clear, opts("Live Filter: Clear"))
   map("n", "f", api.live_filter.start, opts("Live Filter: Start"))
   map("n", "g?", api.tree.toggle_help, opts("Help"))
-  map("n", "gy", api.fs.copy.absolute_path, opts("Copy Absolute Path"))
-  map("n", "ge", api.fs.copy.basename, opts("Copy Basename"))
-  map("n", "H", api.tree.toggle_hidden_filter, opts("Toggle Filter: Dotfiles"))
-  map("n", "I", api.tree.toggle_gitignore_filter, opts("Toggle Filter: Git Ignore"))
-  map("n", "J", api.node.navigate.sibling.last, opts("Last Sibling"))
-  map("n", "K", api.node.navigate.sibling.first, opts("First Sibling"))
-  map("n", "L", api.node.open.toggle_group_empty, opts("Toggle Group Empty"))
+  map("n", "gY", api.fs.copy.absolute_path, opts("Copy Absolute Path"))
+  map("n", "gy", api.fs.copy.basename, opts("Copy Basename"))
+  map("n", "l", api.tree.toggle_hidden_filter, opts("Toggle Filter: Dotfiles"))
+  map("n", "L", api.tree.toggle_gitignore_filter, opts("Toggle Filter: Git Ignore"))
+  map("n", "I", api.node.navigate.sibling.last, opts("Last Sibling"))
+  map("n", "N", api.node.navigate.sibling.first, opts("First Sibling"))
+  map("n", "o", api.node.open.toggle_group_empty, opts("Toggle Group Empty"))
   map("n", "M", api.tree.toggle_no_bookmark_filter, opts("Toggle Filter: No Bookmark"))
   map("n", "m", api.marks.toggle, opts("Toggle Bookmark"))
   map("n", "o", api.node.open.edit, opts("Open"))
-  map("n", "O", api.node.open.no_window_picker, opts("Open: No Window Picker"))
+  map("n", "<C-o>", api.node.open.no_window_picker, opts("Open: No Window Picker"))
   map("n", "p", api.fs.paste, opts("Paste"))
-  map("n", "P", api.node.navigate.parent, opts("Parent Directory"))
+  map("n", "h", api.node.navigate.parent, opts("Parent Directory"))
   map("n", "q", api.tree.close, opts("Close"))
   map("n", "r", api.fs.rename, opts("Rename"))
   map("n", "R", api.tree.reload, opts("Refresh"))
-  map("n", "s", api.node.run.system, opts("Run System"))
-  map("n", "S", api.tree.search_node, opts("Search"))
+  map("n", "S", api.node.run.system, opts("Run System"))
+  map("n", "s", api.tree.search_node, opts("Search"))
   map("n", "f", api.fs.rename_full, opts("Rename: Full Path"))
   map("n", "F", api.tree.toggle_custom_filter, opts("Toggle Filter: Hidden"))
   map("n", "W", api.tree.collapse_all, opts("Collapse All"))
-  map("n", "x", api.fs.cut, opts("Cut"))
+  map("n", "v", api.fs.cut, opts("Cut"))
   map("n", "y", api.fs.copy.filename, opts("Copy Name"))
   map("n", "Y", api.fs.copy.relative_path, opts("Copy Relative Path"))
   map("n", "<2-LeftMouse>", api.node.open.edit, opts("Open"))
   map("n", "<2-RightMouse>", api.tree.change_root_to_node, opts("CD"))
   -- END_DEFAULT_ON_ATTACH
-  -- copy default mappings here from defaults in next section
-  vim.keymap.set("n", "<C-]>", api.tree.change_root_to_node, opts("CD"))
-  vim.keymap.set("n", "<C-e>", api.node.open.replace_tree_buffer, opts("Open: In Place"))
-  ---
-  -- OR use all default mappings
-  api.config.mappings.default_on_attach(bufnr)
-
-  -- remove a default
-  vim.keymap.del("n", "<C-]>", { buffer = bufnr })
-
-  -- override a default
-  vim.keymap.set("n", "<C-e>", api.tree.reload, opts("Refresh"))
-
-  -- add your mappings
-  vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
+  -- -- copy default mappings here from defaults in next section
+  -- vim.keymap.set("n", "<C-]>", api.tree.change_root_to_node, opts("CD"))
+  -- vim.keymap.set("n", "<C-e>", api.node.open.replace_tree_buffer, opts("Open: In Place"))
+  -- ---
+  -- -- OR use all default mappings
+  -- api.config.mappings.default_on_attach(bufnr)
+  --
+  -- -- remove a default
+  -- vim.keymap.del("n", "<C-]>", { buffer = bufnr })
+  --
+  -- -- override a default
+  -- vim.keymap.set("n", "<C-e>", api.tree.reload, opts("Refresh"))
+  --
+  -- -- add your mappings
+  -- vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
   ---
 end
 
